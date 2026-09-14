1cd6f6cf31eb2b768f344d5db60fa0b150c7ae0b
peterciprian <peterciprian@gmail.com>
2026-09-14 15:36:27 +0200

    feat: add UserEntity to stages module imports for enhanced functionality

--- stroll-bar-backend/src/modules/stages/stages.module.ts
+++ stroll-bar-backend/src/modules/stages/stages.module.ts
@@ -5,10 +5,11 @@ import { StagesService } from './stages.service';
 import { StageEntity } from './entities/stage.entity';
 import { StrollEntity } from '../strolls/entities/stroll.entity';
 import { AdventureEntity } from '../adventures/entities/adventure.entity';
+import { UserEntity } from '../users/entities/user.entity';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([StageEntity, StrollEntity, AdventureEntity]), AuthModule],
+	imports: [TypeOrmModule.forFeature([StageEntity, StrollEntity, AdventureEntity, UserEntity]), AuthModule],
 	controllers: [StagesController],
 	providers: [StagesService],
 	exports: [StagesService]
